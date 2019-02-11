b4adc338e2c2fdabcf9cd4e274aa1b6f94760065
chalizardmisadon <chalizardmisadon@yahoo.com>
2019-02-11 03:35:19 -0500

    add Ultrasonic sensor code

---
+++ Ultrasonic_Sensor/Ultrasonic_Sensor.ino
@@ -0,0 +1,15 @@
+#define trigPin 12
+#define echoPin 13
+
+long duration;
+int distance;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
